41ed19b08afa27b13fef72213678346950e3823e
NzrManoe <m.nezer2@rubika-edu.re>
2026-04-03 14:36:49 +0400

    Rajout du menu pause in Game fonctionnel

---
+++ Assets/UIManager.cs
@@ -0,0 +1,60 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [SerializeField] GameObject pausePanel;
+
+    [Header("Input")]
+    [SerializeField] private InputActionReference pauseAction;
+
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        pauseAction.action.performed += OnPause;
+        pauseAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pauseAction.action.performed -= OnPause;
+        pauseAction.action.Disable();
+    }
+
+    private void OnPause(InputAction.CallbackContext context)
+    {
+        TogglePause();
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        pausePanel.SetActive(isPaused);
+
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
+
+    public void quitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
