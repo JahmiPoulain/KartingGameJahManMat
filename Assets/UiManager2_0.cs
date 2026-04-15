bd0594b717989040578d39f3489019e24084993d
NzrManoe <manoenzr@gmail.com>
2026-04-15 11:23:58 +0400

    Add pointer/canvas UI and update UiManager
    Add new UI objects and components to the MainMenu2_0 scene (Canvas, Panel, Pointer, CanvasGroup and related components), adjust canvas render mode and sorting order, and replace an obsolete MonoBehaviour reference. Add .meta entries for new DynamicUICursor and MenuUiManager assets. Update UiManager2_0: add serialized fields, listen for any key to toggle main canvas/UI panel, and add StartLoadingScreen() to enable the loading screen GameObject.

--- Assets/UiManager2_0.cs
+++ Assets/UiManager2_0.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager2_0 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject ButtonsCanva;
+    [SerializeField] private GameObject loadingScreenCanva;
+
+
+
     void Start()
     {
         
@@ -11,13 +16,40 @@ public class UiManager2_0 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            GameObject canvas = GameObject.Find("Canvas");
+            if (canvas != null)
+            {
+                canvas.SetActive(false);
+            }
+
+            GameObject uiPanel = GameObject.Find("UiPanel");
+            if (uiPanel != null)
+            {
+                uiPanel.SetActive(true);
+            }
+        }
+
+
+
     }
 
 
     public void TogglePanel(GameObject panel)
     {
         panel.SetActive(!panel.activeSelf);
+    }
+
+    // start loadingScreen
+    public void StartLoadingScreen()
+    {
+        // Assuming you have a loading screen GameObject in your scene
+        GameObject loadingScreen = GameObject.Find("LoadingScreen");
+        if (loadingScreen != null)
+        {
+            loadingScreen.SetActive(true);
+        }
     }
 
 
