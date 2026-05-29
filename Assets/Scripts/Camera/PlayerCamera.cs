6c3680465734f04470dfe727f4d5d51806011dc9
JahmiPoulain <j.poulain@rubika-edu.re>
2026-05-29 15:14:13 +0400

    suite

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.position += transform.forward;
     }
 }
