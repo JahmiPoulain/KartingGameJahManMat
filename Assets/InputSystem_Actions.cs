01584f62a947511ccec4cf065c9ca3bd953bf5d6
NzrManoe <manoenzr@gmail.com>
2026-06-04 10:24:16 +0400

    Mouahahaha

--- Assets/InputSystem_Actions.cs
+++ Assets/InputSystem_Actions.cs
@@ -216,7 +216,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""id"": ""978bfe49-cc26-4a3d-ab7b-7d7a29327403"",
                     ""path"": ""<Gamepad>/leftStick/x"",
                     ""interactions"": """",
-                    ""processors"": """",
+                    ""processors"": ""Scale"",
                     ""groups"": "";Gamepad"",
                     ""action"": ""Turn"",
                     ""isComposite"": false,
