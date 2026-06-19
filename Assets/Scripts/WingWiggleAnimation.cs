b70e4e71d9cfba581a97adf4e7b18217f1c0c95d
JahmiPoulain <j.poulain@rubika-edu.re>
2026-06-19 12:43:58 +0400

    AILES QUI BATTENT

--- Assets/Scripts/WingWiggleAnimation.cs
+++ Assets/Scripts/WingWiggleAnimation.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class WingWiggleAnimation : MonoBehaviour
 {
-    [SerializeField] bool rightWing;
+   // [SerializeField] bool rightWing;
+    [SerializeField] float speed;
     bool goUp;
-    float maxOffset = 10f;
+    float maxOffset = 0.05f;
+    float rngOffsetTarget;
     float offset;
+    [SerializeField] Transform rightWing;
+    [SerializeField] Transform leftWing;
+    float accel;
+
     void Start()
     {
-        
+        //rngOffsetTarget = Random.Range(0.3f, maxOffset);
     }
 
     void Update()
@@ -18,11 +24,30 @@ public class WingWiggleAnimation : MonoBehaviour
 
     void WiggleWing()
     {
-        if (rightWing)
-        {
-            
-        }
-        offset = Mathf.PingPong(Time.time, maxOffset);
-        transform.localEulerAngles = new Vector3( -90f + offset, 0, 0);
+        //speed = KartScriptV2.instance.flightSpeed * 6f;
+            if (goUp)
+            {
+                accel += speed * Time.deltaTime;
+                if (offset > maxOffset)
+                {
+                    goUp = false;
+                    //rngOffsetTarget = Random.Range(0.3f, maxOffset);
+                }
+            }
+            else
+            {
+                accel -= speed * Time.deltaTime;
+                // offset -= speed * Time.deltaTime;
+                if (offset < -maxOffset)
+                {
+                    goUp = true;
+                    //rngOffsetTarget = Random.Range(0.3f, maxOffset);
+                }
+            }
+        offset += (accel) * Time.deltaTime;
+        leftWing.localEulerAngles = new Vector3(0, 0, -offset);
+        rightWing.localEulerAngles = new Vector3( 0, 0, offset);
+        //offset = Mathf.PingPong(Time.time * 80f, maxOffset);
+       // transform.localEulerAngles = new Vector3( +offset, -90, 90);
     }
 }
