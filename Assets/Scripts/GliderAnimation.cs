383516b2eeffc17198336b1f50c1ed7bb640bb49
JahmiPoulain <j.poulain@rubika-edu.re>
2026-06-20 17:07:08 +0400

    fix 2

--- Assets/Scripts/GliderAnimation.cs
+++ Assets/Scripts/GliderAnimation.cs
@@ -8,7 +8,7 @@ public class GliderAnimation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
